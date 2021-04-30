c7dbc65b82113f9ca4e1f878f8d223b66c946474
TakuyaHarayama <harayama.develop@gmail.com>
2021-04-30 12:37:58 +0900

    create comment form

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,41 @@
+class CommentsController < ApplicationController
+  before_action :set_post
+
+  def index
+    @comments = @post.comments
+  end
+
+  def create
+    @comment = @post.comments.new(comment_params)
+
+    respond_to do |format|
+      if @comment.save
+        format.html { redirect_to @post, notice: "コメントしました" }
+        format.json { render :show, status: :created, location: @post }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+
+    @comment.destroy
+    respond_to do |format|
+      format.html { redirect_to @post, notice: "コメントを削除しました" }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :posts
-  mount LetterOpenerWeb::Engine, at: '/letter_opener' if Rails.env.development?
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :posts do
+    resources :comments, only: %i(index create destroy)
+  end
 end
